4d37bd92c524be03e03c127339942e3590b10557
julioElpabo <christophe.roulet@edufr.ch>
2026-07-30 16:33:51 +0200

    Modif textes contact

--- .output/server/chunks/build/ChallengeWorkspace-styles.DBSpehYC.mjs
+++
@@ -1,8 +0,0 @@
-import { m as main } from './WizardChallengeWorkspace-styles-1.mjs-Bwah0U_3.mjs';
-
-const ChallengeWorkspaceStyles_DBSpehYC = [
-  main
-];
-
-export { ChallengeWorkspaceStyles_DBSpehYC as default };
-//# sourceMappingURL=ChallengeWorkspace-styles.DBSpehYC.mjs.map
---
+++ .output/server/chunks/build/ChallengeWorkspace-styles.DqWyEuwi.mjs
@@ -0,0 +1,8 @@
+import { m as main } from './ChallengeWorkspace-styles-1.mjs-Bwah0U_3.mjs';
+
+const ChallengeWorkspaceStyles_DqWyEuwi = [
+  main
+];
+
+export { ChallengeWorkspaceStyles_DqWyEuwi as default };
+//# sourceMappingURL=ChallengeWorkspace-styles.DqWyEuwi.mjs.map
